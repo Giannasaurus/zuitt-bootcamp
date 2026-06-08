2a561520c5b3a43c6b093d47b7a1b13a1c83d373
giannasaurus <gianzcantor@gmail.com>
2026-06-09 00:19:44 +0800

    Complete s01 activity

---
+++ s01/index.js
@@ -0,0 +1,37 @@
+const firstName = "Gianna";
+const lastName = "Cantor";
+let age = 19;
+let hobbies = ["Coding", "Learning", "Designing"];
+let myPlaylist = ["Sun and Moon", "Don't Let Me Go", "Melody"];
+let myMovies = ["The Maze Runner", "The Death Cure", "The Scorch Trials"];
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(myPlaylist);
+console.log(myMovies);
+
+const game1 = {
+	name: "Bloons TD 6",
+	createdOn: "June 13, 2018",
+	publisher: "Ninja Kiwi",
+	totalSales: 8500000,
+	isAvailable: true
+}
+
+const game2 = {
+	name: "Evil Genius 2",
+	createdOn: "March 30, 2021",
+	publisher: "Rebellion",
+	totalSales: 400000,
+	isAvailable: true
+}
+
+const game3 = {
+	name: "Oxygen Not Included",
+	createdOn: "July 30, 2019",
+	publisher: "Klei Entertainment",
+	totalSales: 2400000,
+	isAvailable: true
+}
